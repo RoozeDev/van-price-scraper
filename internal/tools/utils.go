86d590b44523462681b65bf4fbfc6884bd9c9ce1
ojp13 <32867061+ojp13@users.noreply.github.com>
2025-03-17 11:54:08 +0000

    added GetDatabaseName util as it was required for API

--- internal/tools/utils.go
+++ internal/tools/utils.go
@@ -25,10 +25,15 @@ func ReadSecretAsString(secret string) string {
 	return ReadFileAsString(filePath)
 }
 
+func GetDatabaseName() string {
+	var database = ReadSecretAsString("db_name")
+	return database
+}
+
 func GetDatabaseConnectionURL() string {
 	var username = ReadSecretAsString("db_username")
 	var password = ReadSecretAsString("db_password")
-	var database = ReadSecretAsString("db_name")
+	var database = GetDatabaseName()
 
 	var uri = "mongodb://" + username + ":" + password + "@mongo:27017/" + database
 
