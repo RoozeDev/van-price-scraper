c5dfab0b393686f2b490bbc7a3ce5310f5f94925
ojp13 <32867061+ojp13@users.noreply.github.com>
2025-03-17 11:47:38 +0000

    simplified some utils

--- internal/tools/utils.go
+++ internal/tools/utils.go
@@ -25,15 +25,10 @@ func ReadSecretAsString(secret string) string {
 	return ReadFileAsString(filePath)
 }
 
-func GetDatabaseName() string {
-	var database = ReadSecretAsString("db_name")
-	return database
-}
-
 func GetDatabaseConnectionURL() string {
 	var username = ReadSecretAsString("db_username")
 	var password = ReadSecretAsString("db_password")
-	var database = GetDatabaseName()
+	var database = ReadSecretAsString("db_name")
 
 	var uri = "mongodb://" + username + ":" + password + "@mongo:27017/" + database
 
